8781f170eb29998c8c40c2142d6648615e434d43
atulnitrr <atulkumar458@gmail.com>
2019-07-27 00:47:57 +0530

    Dependency

--- src/main/java/com/atul/setup/setup/SetupApplication.java
+++ src/main/java/com/atul/setup/setup/SetupApplication.java
@@ -1,10 +1,15 @@
 package com.atul.setup.setup;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SetupApplication {
+public class SetupApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SetupApplication.class, args);
@@ -12,4 +17,14 @@ public class SetupApplication {
 		System.out.println("App is running");
 	}
 
+
+
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Override
+	public void run(final String... args) throws Exception {
+		rabbitTemplate.convertAndSend("Hello from our first message ");
+	}
 }
